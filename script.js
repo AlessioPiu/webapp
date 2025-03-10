55265ded2360255957cb40df855a0ae7362b8842
AlessioPiu <noobpiciu@gmail.com>
2025-03-10 16:43:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let message = document.getElementById("message");
-    if (message) {
-        setTimeout(() => {
-            message.style.display = "none";
-        }, 3000);
-    }
-});
